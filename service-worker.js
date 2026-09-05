7beeac1e67e7f453412ceb86f24a737b1a573e5b
Mark-Keh <bestmanchristian768@gmail.com>
2026-09-05 13:23:45 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,47 @@
-const CACHE_NAME = "chip-bread-manager-v1";
+```javascript
+const CACHE_NAME = "chip-bread-manager-v2";
 
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
     "./chips.css",
     "./chips.js",
-    "./manifest.json"
+    "./manifest.json?v=2"
 ];
 
 self.addEventListener("install", function(event) {
-
     event.waitUntil(
-
         caches.open(CACHE_NAME).then(function(cache) {
-
             return cache.addAll(FILES_TO_CACHE);
-
         })
-
     );
 
+    self.skipWaiting();
 });
 
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames
+                    .filter(function(cacheName) {
+                        return cacheName !== CACHE_NAME;
+                    })
+                    .map(function(cacheName) {
+                        return caches.delete(cacheName);
+                    })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
 
 self.addEventListener("fetch", function(event) {
-
     event.respondWith(
-
         caches.match(event.request).then(function(response) {
-
             return response || fetch(event.request);
-
         })
-
     );
-
 });
+```
